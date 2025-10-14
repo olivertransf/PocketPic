8984706f2a32efd8ed313a4a6d49f4732e46ef65
Oliver Tran <olivertransf@gmail.com>
2025-10-14 11:02:14 -0700

    Initial Commit

---
+++ PocketPic/PocketPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPicApp.swift
+//  PocketPic
+//
+//  Created by Oliver Tran on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
